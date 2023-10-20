351da56f2d92e6893d9c6c201e777af90509adc6
Dababy110 <vargasariasdavid110@gmail.com>
2023-10-20 12:55:26 -0600

    second

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,41 +1,35 @@
-import { useState } from "react";
+import EatonLogoWithout from "../assets/img/EatonLogoWithout.png";
 
 const Home = () => {
-  const [isChecked, setIsChecked] = useState(false);
-
-  const handleToggle = () => {
-    setIsChecked(!isChecked);
-  };
-
   return (
-    <div className="flex items-center justify-center m-14 mb-0 w-full">
-      <div>
-        <label htmlFor="toggle" className="cursor-pointer w-full h-full">
-          <div
-            className={`flex items-center justify-center relative w-full h-[10em] rounded-xl transition duration-300 ease-in-out ${
-              isChecked ? "bg-green-400" : "bg-gray-300"
-            }`}
-          >
+    <>
+      <div className="flex flex-col items-center justify-center m-14 mb-0 w-full">
+        <div>
+          <div className="cursor-pointer w-full">
             <div
-              className={`absolute w-[10em] h-6 bg-white border-2 border-gray-400 rounded-full transition-transform duration-300 ease-in-out transform ${
-                isChecked ? "translate-x-[0em]" : "translate-x-[-1em]"
-              }`}
+              className={`group flex items-center justify-center relative w-full h-[10em] rounded-xl transition duration-300 ease-in-out`}
             >
-              sdajs
+              <div
+                className={`absolute top-11 right-3 w-[11.1em] h-[3.9em] bg-[#157CC4] transition-transform duration-300 ease-in-out transform translate-x-[0em] group-hover:translate-x-[-11.1em] rounded-md`}
+              ></div>
+              <h1 className="text-[#157CC4] text-[4em] font-abc font-semibold text-center z-10 duration-300 ease-in-out group-hover:text-white">
+                AQUA
+              </h1>
+              <h1 className="text-white text-[4em] font-abc font-semibold m-5 text-center z-10 duration-300 ease-in-out group-hover:text-[#157CC4]">
+                SYNC
+              </h1>
             </div>
-            <h1 className="text-[#157CC4] text-[4em] font-abc font-semibold text-center">AQUA</h1>
-            <h1 className="text-[#157CC4] text-[4em] font-abc font-semibold m-5 text-center">SYNC</h1>
           </div>
-        </label>
-        <input
-          type="checkbox"
-          id="toggle"
-          className="hidden"
-          checked={isChecked}
-          onChange={handleToggle}
-        />
+          <p className="text-center mt-[-1em]">
+            Connecting Your Life with the Ideal Water.
+          </p>
+        </div>
       </div>
-    </div>
+      <div className="flex gap-3 justify-end p-10">
+        <h1 className="font-bold text-lg font-abc">By</h1>
+        <img src={EatonLogoWithout} className="w-[10em] h-[2em] mt-4" alt="" />
+      </div>
+    </>
   );
 };
 
